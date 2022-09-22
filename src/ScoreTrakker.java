7821d3066ab6b3991e2f9da873955e9579ccaf02
Brian Burr <bburr@mines.edu>
2022-09-22 16:03:45 -0600

    Added text files and outline for ScoreTrakker

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,4 +1,21 @@
+import java.util.*;
+import java.io.*;
 
 public class ScoreTrakker {
-
+	private ArrayList<Student> students;
+	private String[] files = {"scores.txt", "badscore.txt"};
+	
+	private void loadDataFile(String fileName) {
+		try {
+		FileReader reader = new FileReader(fileName);
+		
+		
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		ScoreTrakker s = new ScoreTrakker();
+	}
 }
