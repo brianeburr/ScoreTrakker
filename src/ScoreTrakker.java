24ac33a55fc1d6803f60fcb4b23c7520e3c5a666
Brian Burr <bburr@mines.edu>
2022-09-22 16:50:08 -0600

    Finished implementing ScoreTrakker

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -3,10 +3,14 @@ import java.io.*;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
-	private String[] files = {"scores.txt", "badscore.txt"};
+	private String[] files  = {"scores.txt", "badscore.txt", "nofile.txt"};
 	
-	private void loadDataFile(String fileName) throws FileNotFoundException{
-		
+	public ScoreTrakker() {
+		students = new ArrayList<Student>();
+	}
+	
+	public void loadDataFile(String fileName) throws FileNotFoundException{
+		students.clear();
 		FileReader reader = new FileReader(fileName);
 		Scanner fileScanner = new Scanner(reader);
 		String name = "", scoreBeforeInt = "";
@@ -20,14 +24,34 @@ public class ScoreTrakker {
 			} 
 			catch (NumberFormatException e) {
 				System.out.println("Incorrect format for " + name + " not a valid score: " + scoreBeforeInt);
+				System.out.println();
 			}
 		}
 		fileScanner.close();
 	}
 	
+	public void printInOrder() {
+		students.sort(null);
+		for (Student student : students) {
+			System.out.println(student);
+		}
+		System.out.println();
+	}
 	
+	public void processFiles() throws FileNotFoundException {
+		for (String file : files) {
+			try {
+				loadDataFile(file);
+				printInOrder();
+			} catch (FileNotFoundException e) {
+				System.out.println("Can't open file: " + file);
+			}
+
+		}
+	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		ScoreTrakker s = new ScoreTrakker();
+		s.processFiles();
 	}
 }
