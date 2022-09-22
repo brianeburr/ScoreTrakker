7598268e7daed54d1b5c8c915bcf01ed18b417a9
Brian Burr <bburr@mines.edu>
2022-09-22 16:33:01 -0600

    Creating loadDataFile function

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -5,15 +5,27 @@ public class ScoreTrakker {
 	private ArrayList<Student> students;
 	private String[] files = {"scores.txt", "badscore.txt"};
 	
-	private void loadDataFile(String fileName) {
-		try {
+	private void loadDataFile(String fileName) throws FileNotFoundException{
+		
 		FileReader reader = new FileReader(fileName);
+		Scanner fileScanner = new Scanner(reader);
+		String name = "", scoreBeforeInt = "";
+		while (fileScanner.hasNext()) {
+			try {
+				name = fileScanner.nextLine();
+				scoreBeforeInt = fileScanner.nextLine();
+				int score = Integer.parseInt(scoreBeforeInt);
+				students.add(new Student(name, score));
 		
-		
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
+			} 
+			catch (NumberFormatException e) {
+				System.out.println("Incorrect format for " + name + " not a valid score: " + scoreBeforeInt);
+			}
 		}
+		fileScanner.close();
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		ScoreTrakker s = new ScoreTrakker();
