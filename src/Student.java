b240fdcce66d9fc33cf7c2cd2e1e41514e6a6c08
Brian Burr <bburr@mines.edu>
2022-09-22 16:19:09 -0600

    implemented student class

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,30 @@
 
-public class Student {
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+	
+	public Student(String name, int score) { // student class instantiated with name as first last
+		this.name = name;
+		this.score = score;
+	}
 
+	@Override
+	public int compareTo(Student o) {
+		return name.compareTo(o.getName()); // compare by name
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return  name +  " " +score;
+	}
+	
+	
+	
+	
+	
+	
 }
